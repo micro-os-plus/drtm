1c4a155d39c18290d49dfe8726db97fd865fd88c
Liviu Ionescu <ilg@livius.net>
2017-03-21 21:03:03 +0200

    add samples/drtm-*.h

---
+++ samples/drtm-backend.h
@@ -0,0 +1,437 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2017 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * This sample file can be used to prepare a custom backend
+ * for the DRTM library.
+ *
+ * It should provide the DRTM library access to the underlying
+ * GDB server.
+ *
+ * If the support functions are global symbols, they can be
+ * called directly, without any pointer forwarders.
+ */
+
+#include <stdio.h>
+
+#if defined(__cplusplus)
+
+#include <cstring>
+#include <cassert>
+#include <cstdarg>
+
+namespace your_namespace
+{
+  namespace drtm
+  {
+
+    /**
+     * This template class provides an implementation for the DRTM backend
+     * that forwards calls to the <your application> SDK C API.
+     *
+     * @tparam S type of the server API forwarder, if any
+     * @tparam Y type of the symbol structure, with name & address
+     */
+    template<typename S, typename Y>
+      class backend
+      {
+      public:
+
+        constexpr static std::size_t tmp_buf_size_bytes = 256;
+        using server_api_type = S;
+        using symbols_type = Y;
+        using target_addr_t = typename Y::element_type;
+
+      public:
+        /**
+         * @brief Construct a <your application> backend.
+         *
+         * @param api Pointer to C API.
+         */
+        backend (const server_api_type* api, const symbols_type* symbols) :
+            api_ (api), symbols_ (symbols)
+        {
+#if defined(DEBUG)
+          printf ("%s(%p, %p) @%p\n", __func__, api, symbols, this);
+#endif /* defined(DEBUG) */
+          ;
+        }
+
+        // The rule of five.
+        backend (const backend&) = delete;
+        backend (backend&&) = delete;
+        backend&
+        operator= (const backend&) = delete;
+        backend&
+        operator= (backend&&) = delete;
+
+        ~backend () = default;
+
+      public:
+
+        target_addr_t
+        get_symbol_address (const char* name)
+        {
+          assert(name != nullptr);
+
+          const symbols_type* p = symbols_;
+          for (; p->name; ++p)
+            {
+              if (std::strcmp (name, p->name) == 0)
+                {
+                  return p->address;
+                }
+            }
+
+          return 0;
+        }
+
+        /**
+         * @brief Output a formatted log message to the
+         * <your application> server window.
+         *
+         * @details
+         * If a log file is specified, the message will also be printed to
+         * the log file.
+         */
+        int
+        output (const char* fmt, ...)
+        {
+          std::va_list args;
+          va_start(args, fmt);
+
+          int ret = voutput (fmt, args);
+
+          va_end(args);
+          return ret;
+        }
+
+        int
+        voutput (const char* fmt, va_list args)
+        {
+          char buf[tmp_buf_size_bytes];
+
+          vsnprintf (buf, sizeof(buf), fmt, args);
+          api_->output (buf);
+
+          return std::strlen (buf);
+        }
+
+        /**
+         * @brief Output a formatted log message to the debug channel.
+         *
+         * @details
+         * Outputs to the debug channel are suppressed in non-debug builds of
+         * the <your application> server.
+         */
+        int
+        output_debug (const char* fmt, ...)
+        {
+          std::va_list args;
+          va_start(args, fmt);
+
+          int ret = voutput_debug (fmt, args);
+
+          va_end(args);
+          return ret;
+        }
+
+        int
+        voutput_debug (const char* fmt, std::va_list args)
+        {
+          char buf[tmp_buf_size_bytes];
+
+          vsnprintf (buf, sizeof(buf), fmt, args);
+          api_->output_debug (buf);
+
+          return std::strlen (buf);
+        }
+
+        /**
+         * @brief Output a formatted warning message to the
+         * <your application> server window.
+         *
+         * @details
+         * The line starts with “WARNING: ”. If a log file is specified, the
+         * message will also be printed to the log file.
+         */
+        int
+        output_warning (const char* fmt, ...)
+        {
+          std::va_list args;
+          va_start(args, fmt);
+
+          int ret = voutput_warning (fmt, args);
+
+          va_end(args);
+          return ret;
+        }
+
+        int
+        voutput_warning (const char* fmt, va_list args)
+        {
+          char buf[tmp_buf_size_bytes];
+
+          vsnprintf (buf, sizeof(buf), fmt, args);
+          api_->output_warning (buf);
+
+          return std::strlen (buf);
+        }
+
+        /**
+         * @brief Output a formatted error message to the
+         * <your application> server window.
+         *
+         * @details
+         * The line starts with “ERROR: ”. If a log file is specified, the
+         * message will also be printed to the log file.
+         */
+        int
+        output_error (const char* fmt, ...)
+        {
+          std::va_list args;
+          va_start(args, fmt);
+
+          int ret = voutput_error (fmt, args);
+
+          va_end(args);
+          return ret;
+        }
+
+        int
+        voutput_error (const char* fmt, va_list args)
+        {
+          char buf[tmp_buf_size_bytes];
+
+          vsnprintf (buf, sizeof(buf), fmt, args);
+          api_->output_error (buf);
+
+          return std::strlen (buf);
+        }
+
+        /**
+         * @brief Read memory from the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to read from.
+         * @param [out] in_array Pointer to buffer for target memory.
+         * @param [in] nbytes Number of bytes to read.
+         *
+         * @retval 0 Reading memory OK.
+         * @retval <0 Reading memory failed.
+         */
+        inline int
+        read_byte_array (target_addr_t addr, uint8_t* in_array,
+                         std::size_t nbytes)
+        {
+          return api_->read_byte_array (addr, in_array, nbytes);
+        }
+
+        /**
+         * @brief Read one byte from the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to read from.
+         * @param [out] in_byte Pointer to byte.
+         *
+         * @retval 0 Reading memory OK.
+         * @retval <0 Reading memory failed.
+         */
+        inline int
+        read_byte (target_addr_t addr, uint8_t* in_byte)
+        {
+          return api_->read_byte (addr, in_byte);
+        }
+
+        /**
+         * @brief Read two bytes from the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to read from.
+         * @param [out] in_byte Pointer to two bytes.
+         *
+         * @retval 0 Reading memory OK.
+         * @retval <0 Reading memory failed.
+         */
+        inline int
+        read_short (target_addr_t addr, uint16_t* in_short)
+        {
+          return api_->read_short (addr, in_short);
+        }
+
+        /**
+         * @brief Read four bytes from the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to read from.
+         * @param [out] in_byte Pointer to four bytes.
+         *
+         * @retval 0 Reading memory OK.
+         * @retval <0 Reading memory failed.
+         */
+        inline int
+        read_long (target_addr_t addr, uint32_t* in_long)
+        {
+          return api_->read_long (addr, in_long);
+        }
+
+        /**
+         * @brief Write memory to the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to write to.
+         * @param [in] out_array Pointer to buffer for target memory.
+         * @param [in] nbytes Number of bytes to write.
+         *
+         * @retval 0 Writing memory OK.
+         * @retval <0 Writing memory failed.
+         */
+        inline int
+        write_byte_array (target_addr_t addr, const uint8_t* out_array,
+                          std::size_t nbytes)
+        {
+          return api_->write_byte_array (addr, out_array, nbytes);
+        }
+
+        /**
+         * @brief Write one byte to the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to write to.
+         * @param [in] out_byte Byte to write.
+         *
+         * @retval 0 Writing memory OK.
+         * @retval <0 Writing memory failed.
+         */
+        inline void
+        write_byte (target_addr_t addr, uint8_t out_byte)
+        {
+          api_->write_byte (addr, out_byte);
+        }
+
+        /**
+         * @brief Write two bytes to the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to write to.
+         * @param [in] out_short Bytes to write.
+         *
+         * @retval 0 Writing memory OK.
+         * @retval <0 Writing memory failed.
+         */
+        inline void
+        write_short (target_addr_t addr, uint16_t out_short)
+        {
+          api_->write_short (addr, out_short);
+        }
+
+        /**
+         * @brief Write four bytes to the target system.
+         *
+         * @details
+         * If necessary, the target CPU is halted in order to read memory.
+         *
+         * @param [in] addr Target address to write to.
+         * @param [in] out_long Bytes to write.
+         *
+         * @retval 0 Writing memory OK.
+         * @retval <0 Writing memory failed.
+         */
+        inline void
+        write_long (target_addr_t addr, uint32_t out_long)
+        {
+          api_->write_long (addr, out_long);
+        }
+
+        /**
+         * @brief Load two bytes from a memory buffer according to the
+         * target endianness.
+         *
+         * @param [in] p Pointer to memory buffer.
+         *
+         * @return The converted value.
+         */
+        inline uint32_t
+        load_short (const uint8_t* p)
+        {
+          return api_->load_short (p);
+        }
+
+        /**
+         * @brief Load three bytes from a memory buffer according to the
+         * target endianness.
+         *
+         * @param [in] p Pointer to memory buffer.
+         *
+         * @return The converted value.
+         */
+        inline uint32_t
+        load_3bytes (const uint8_t* p)
+        {
+          return api_->load_3bytes (p);
+        }
+
+        /**
+         * @brief Load four bytes from a memory buffer according to the
+         * target endianness.
+         *
+         * @param [in] p Pointer to memory buffer.
+         *
+         * @return The converted value.
+         */
+        inline uint32_t
+        load_long (const uint8_t* p)
+        {
+          return api_->load_long (p);
+        }
+
+      private:
+        const server_api_type* api_;
+        const symbols_type* symbols_;
+      };
+
+    ;
+  // Avoid formatter bug
+  // ==========================================================================
+  } /* namespace drtm */
+} /* namespace your_namespace */
+
+#endif /* defined(__cplusplus) */
---
+++ samples/drtm-memory.h
@@ -0,0 +1,300 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2017 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * This sample file can be used to prepare a custom memory manager
+ * for the DRTM library. It is mandatory when integrating the
+ * DRTM library in multi-threading environments that redefine the
+ * standard malloc()/free() with thread save version.
+ *
+ * If not, simply use the std::allocator() when instantiating the
+ * DRTM class templates.
+ *
+ * If the backend memory management functions are global symbols,
+ * it is perfectly possible to call them directly, without any
+ * pointer forwarders.
+ *
+ * If the backend memory management is accessible via some
+ * forwarding pointers, use the second version, where the
+ * server_api_type must define two functions, with prototypes
+ * similar to malloc()/free().
+ */
+
+#include <stdio.h>
+
+#if defined(__cplusplus)
+
+#include <drtm/memory.h>
+
+#define USE_STATIC_ALLOCATOR
+
+namespace your_namespace
+{
+  namespace drtm
+  {
+
+#if defined(USE_STATIC_ALLOCATOR)
+
+    extern void*
+    your_application_malloc (std::size_t);
+
+    extern void
+    your_application_free (void*);
+
+    /**
+     * @brief A standard allocator that allocates memory via
+     * the backend memory management functions.
+     *
+     * @tparam T type of allocator values
+     */
+    template<typename T>
+      class allocator
+      {
+      public:
+
+        // Standard types.
+        using value_type = T;
+
+      public:
+
+        /**
+         * @brief Construct an allocator object instance.
+         */
+        allocator () noexcept
+        {
+#if defined(DEBUG)
+          printf ("%s() @%p\n", __func__, this);
+#endif /* defined(DEBUG) */
+        }
+
+        /**
+         * @brief Copy construct an allocator object instance.
+         */
+        allocator (allocator const & a) = default;
+
+        /**
+         * @brief Construct a sibling allocator object instance,
+         * for a different type.
+         */
+        template<typename U>
+          allocator (allocator<U> const & other) noexcept
+          {
+            ;
+          }
+
+        /**
+         * @brief Assign an allocator object instance.
+         */
+        allocator&
+        operator= (allocator const & a) = default;
+
+        /**
+         * @brief Allocate a number of objects of the allocator type.
+         */
+        value_type*
+        allocate (std::size_t objects)
+        {
+#if defined(DEBUG)
+          printf ("%s(%zu) %p\n", __func__, objects, this);
+#endif /* defined(DEBUG) */
+
+          if (objects > max_size ())
+            {
+              throw std::system_error (
+                  std::error_code (EINVAL, std::system_category ()));
+            }
+
+          value_type*p = static_cast<value_type*> (your_application_malloc (
+              objects * sizeof(value_type)));
+
+#if defined(DEBUG)
+          printf ("%s(%zu)=%p %p\n", __func__, objects, p, this);
+#endif /* defined(DEBUG) */
+
+          return p;
+        }
+
+        /**
+         * @brief Deallocate the number of objects.
+         */
+        void
+        deallocate (value_type* p, std::size_t objects) noexcept
+        {
+#if defined(DEBUG)
+          printf ("%s(%p,%zu) %p\n", __func__, p, objects, this);
+#endif /* defined(DEBUG) */
+
+          assert(objects <= max_size ());
+          your_application_free (p);
+        }
+
+        /**
+         * @brief Get the maximum number of objects that can be allocated.
+         */
+        std::size_t
+        max_size (void) const noexcept
+        {
+          return std::numeric_limits<std::size_t>::max () / sizeof(value_type);
+        }
+
+        allocator
+        select_on_container_copy_construction (void) const noexcept
+        {
+          return allocator ();
+        }
+
+      private:
+
+        // None.
+      };
+
+#else
+
+    /**
+     * @brief A standard allocator that allocates memory via
+     * the backend memory management functions.
+     *
+     * @tparam T type of allocator values
+     * @tparam S type of the server API, if any.
+     */
+    template<typename T, typename S>
+    class allocator
+      {
+      public:
+
+        // Standard types.
+        using value_type = T;
+
+        // Custom type.
+        using server_api_type = S;
+
+      public:
+
+        /**
+         * @brief Construct an allocator object instance.
+         */
+        allocator (const server_api_type* api) noexcept
+          {
+#if defined(DEBUG)
+            printf ("%s(%p) @%p\n", __func__, api, this);
+#endif /* defined(DEBUG) */
+
+            api_ = api;
+          }
+
+        /**
+         * @brief Copy construct an allocator object instance.
+         */
+        allocator (allocator const & a) = default;
+
+        /**
+         * @brief Construct a sibling allocator object instance, of a different type.
+         */
+        template<typename U>
+        allocator (allocator<U, S> const & other, const server_api_type* api) noexcept
+          {
+            api_ = api;
+          }
+
+        /**
+         * @brief Assign an allocator object instance.
+         */
+        allocator&
+        operator= (allocator const & a) = default;
+
+        /**
+         * @brief Allocate a number of objects of the allocator type.
+         */
+        value_type*
+        allocate (std::size_t objects)
+          {
+#if defined(DEBUG)
+            printf ("%s(%zu) %p\n", __func__, objects, this);
+#endif /* defined(DEBUG) */
+
+            if (objects > max_size ())
+              {
+                throw std::system_error (
+                    std::error_code (EINVAL, std::system_category ()));
+              }
+
+            value_type*p = static_cast<value_type*> (api_->malloc (
+                    objects * sizeof(value_type)));
+
+#if defined(DEBUG)
+            printf ("%s(%zu)=%p %p\n", __func__, objects, p, this);
+#endif /* defined(DEBUG) */
+
+            return p;
+          }
+
+        /**
+         * @brief Deallocate the number of objects.
+         */
+        void
+        deallocate (value_type* p, std::size_t objects) noexcept
+          {
+#if defined(DEBUG)
+            printf ("%s(%p,%zu) %p\n", __func__, p, objects, this);
+#endif /* defined(DEBUG) */
+
+            assert(objects <= max_size ());
+            api_->free (p);
+          }
+
+        /**
+         * @brief Get the maximum number of objects that can be allocated.
+         */
+        std::size_t
+        max_size (void) const noexcept
+          {
+            return std::numeric_limits<std::size_t>::max () / sizeof(value_type);
+          }
+
+        allocator
+        select_on_container_copy_construction (void) const noexcept
+          {
+            return allocator (api_);
+          }
+
+      private:
+
+        /**
+         * @brief Pointer to backend forwarders, if any.
+         */
+        const server_api_type* api_;
+      };
+
+#endif
+
+    ;
+  // Avoid formatter bug
+  // ==========================================================================
+  } /* namespace drtm */
+} /* namespace your_namespace */
+
+#endif /* defined(__cplusplus) */
